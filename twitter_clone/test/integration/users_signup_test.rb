4784b1780ed064de0d296751a5667b8d9c7dd8d2
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-10-14 12:14:27 +0900

    Add login screen

--- twitter_clone/test/integration/users_signup_test.rb
+++ twitter_clone/test/integration/users_signup_test.rb
@@ -7,7 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
 
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name:  "",
+      post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
