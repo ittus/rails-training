b279d603c33b00763712890a3e27fcd2fbc6931d
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-10-28 16:39:17 +0900

    Implement uploading image

--- twitter_clone/db/schema.rb
+++ twitter_clone/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_074057) do
+ActiveRecord::Schema.define(version: 2018_10_28_065358) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
