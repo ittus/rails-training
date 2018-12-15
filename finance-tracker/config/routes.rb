0a515512d8133c52322116c9bb85cbb4df969eaf
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-12-15 17:53:26 +0900

    Add and remove friends

--- finance-tracker/config/routes.rb
+++ finance-tracker/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
   get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
