341c8585a07b0c12c25764d710412796bc4c6ca3
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-12-24 18:26:12 +0900

    Add messages model

--- message_me/db/schema.rb
+++ message_me/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_091806) do
+ActiveRecord::Schema.define(version: 2018_12_24_092320) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
