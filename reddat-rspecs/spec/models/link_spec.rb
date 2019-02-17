e8440f14fe80eea011c0652e9c62c2bdf2168972
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-02-17 17:17:30 +0900

    Sort post by number of upvotes - downvotes

--- reddat-rspecs/spec/models/link_spec.rb
+++ reddat-rspecs/spec/models/link_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
+RSpec.describe Link, "validations" do
+  it {is_expected.to validate_presence_of(:title)}
+  it {is_expected.to validate_presence_of(:url)}
+  xit {is_expected.to validate_uniqueness_of(:url)}
+end
+
+
+RSpec.describe Link, ".hottest_first" do
+  it "returns the links: hottest to coldest" do
+    coldest_link = create(:link, upvotes: 3, downvotes: 3)
+    hottest_link = create(:link, upvotes: 5, downvotes: 1)
+    lukewarm_link = create(:link, upvotes: 2, downvotes: 1)
+
+    expect(Link.hottest_first).to eq [hottest_link, lukewarm_link, coldest_link]
+  end
+end
+
 RSpec.describe Link, "#upvote" do
   it "increments upvotes" do
     link = build(:link, upvotes: 1)
