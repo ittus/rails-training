458e3768d42a1946c8eda190ceb72e9b8b2f1cfd
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-02-17 20:56:59 +0900

    Add index and create api

--- reddat-rspecs/config/routes.rb
+++ reddat-rspecs/config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
     resource :upvote, only: [:create]
     resource :downvote, only: [:create]
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :links, only: [:index, :create]
+    end
+  end
 end
